e406c9c3c44799606a56c53b4db26dc672747d49
aliburakgl <61741297+aliburakgl@users.noreply.github.com>
2022-02-07 01:04:41 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,7 +19,7 @@ m = PyMouse()
 cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor("") #dat file directory 
 
 def midpoint(p1, p2):
 	return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
